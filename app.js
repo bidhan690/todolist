959e9ad497d61ee9b3c1635351551010c4785ef5
bidhan <88bidhanniroula@gmail.com>
2023-01-13 18:14:37 +0545

    upd

--- app.js
+++ app.js
@@ -102,6 +102,6 @@ app.get('/about', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Server started on port 3000');
 });
